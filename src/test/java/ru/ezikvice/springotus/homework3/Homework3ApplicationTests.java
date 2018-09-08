2cbde6c4f7475b26c1d2eb4174cdcb3c55052345
ezikvice <shamparov@gmail.com>
2018-09-09 01:02:51 +0300

    tests

--- src/test/java/ru/ezikvice/springotus/homework3/Homework3ApplicationTests.java
+++ src/test/java/ru/ezikvice/springotus/homework3/Homework3ApplicationTests.java
@@ -1,16 +1,76 @@
 package ru.ezikvice.springotus.homework3;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.ezikvice.springotus.domain.Answer;
+import ru.ezikvice.springotus.domain.ExaminationQuestion;
+import ru.ezikvice.springotus.domain.Question;
+import ru.ezikvice.springotus.domain.UserExamination;
+import ru.ezikvice.springotus.service.LoadService;
+import ru.ezikvice.springotus.service.QAService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Homework3ApplicationTests {
 
+    @MockBean
+    QAService mockQaSService;
+
+    @Autowired
+    LoadService loadService;
+
     @Test
-    public void contextLoads() {
+    public void testLoad() {
+        Map<Integer, Question> questionMap = loadService.loadQuestions();
+        Assert.assertTrue(!questionMap.isEmpty());
+        Assert.assertEquals(questionMap.size(), 5);
+
+        //TODO: negative tests
     }
+
+    @Test
+    public void testUserExamination() {
+
+        Map<Integer, Question> questionMap = new HashMap<>();
+
+        for (int q = 0; q < 5; q++) {
+            Map<Integer, Answer> answers = new HashMap<>();
+            for (int a = 0; a < 5; a++) {
+                Answer answer = new Answer(a, q, "testAnswer", a % 2 == 0);
+                answers.put(a, answer);
+            }
+            Question question = new Question(q, "test question", answers);
+            questionMap.put(q, question);
+        }
+
+        List<ExaminationQuestion> questions = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            ExaminationQuestion q = new ExaminationQuestion(i, i, i % 2 == 0);
+            questions.add(q);
+        }
+        UserExamination userExamination = new UserExamination("testUser", questions);
+
+        given(this.mockQaSService.examine(questionMap))
+                .willReturn(userExamination);
+
+        UserExamination examineResults = mockQaSService.examine(questionMap);
+
+        Assert.assertNotNull(examineResults);
+        Assert.assertTrue(!examineResults.getUserQuestions().isEmpty());
+        Assert.assertEquals(examineResults.getUserQuestions().size(), 5);
+    }
+
 
 }
